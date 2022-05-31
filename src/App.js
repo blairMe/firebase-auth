98a4d2c7f1a0c665403ce16c956685c177f6e557
blairMe <felixanachoni@gmail.com>
2022-06-01 02:40:16 +0300

    firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { useRef } from "react";
 
-import './App.css';
-import Login from './components/Login/Login';
+import "./App.css";
+import { signUp } from "./firebase";
 
 function App() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const handleSignUp = async () => {
+    await signUp(emailRef.current.value, passwordRef.current.value);
+  };
+
   return (
-    <div className="App">
-      <Login />
+    <div id="main">
+      <div id="fields">
+        <input ref={emailRef} type="text" placeholder="Email" />
+        <input ref={passwordRef} type="password" placeholder="Password" />
+      </div>
+      <button onClick={handleSignUp}>Sign Up</button>
     </div>
   );
 }
